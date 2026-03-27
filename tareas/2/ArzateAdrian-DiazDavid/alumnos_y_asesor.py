a136a409b4aaf5a0f6113fbb97a736510c561110
David <david@pc-bd-dda.fi.unam>
2026-03-26 23:25:07 -0600

    Se agregaron comentarios sobre el funcionamiento y se agregaron constantes globales para controlar los limites sin dificultad

--- tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
+++ tareas/2/ArzateAdrian-DiazDavid/alumnos_y_asesor.py
@@ -6,6 +6,7 @@ import time
 
 numero_sillas = 10
 
+maximo_dudas = 10
 
 
 class Alumno(th.Thread):
@@ -16,7 +17,7 @@ class Alumno(th.Thread):
 		self.silla = sillas
 		self.turno = turno
 		self.condicion = condicion
-		self.numero_dudas = random.randint(1, 10)
+		self.numero_dudas = random.randint(1, maximo_dudas) # una cantidad de dudas aleatoria entre 1 y maximo_dudas
 
 	# comportamiento del hilo Alumno
 	def run(self):
@@ -43,10 +44,12 @@ class Alumno(th.Thread):
 		print(f"El alumno {self.id} resolvio todas sus dudas") 
 
 class Profesor(th.Thread):
+	# Constructor de la clase Profesor
 	def __init__(self, condicion):
 		super().__init__()
 		self.condicion = condicion
 
+	# comportamiento del hilo profesor
 	def run(self):
 		with self.condicion:
 			while True:
@@ -59,27 +62,30 @@ class Profesor(th.Thread):
 				print(f"El Profesor terminó con una duda")		
 
 def main():
-	
-	numero_sillas = th.Semaphore(10)
-
+	global numero_sillas
+	# semaforo del numero de sillas
+	numero_sillas = th.Semaphore(numero_sillas)
+	# mutex 
 	turno = th.Lock()
-
+	# condicion de espera (dormir)
 	condicion = th.Condition(turno)
-	
+	# inicializacion de los identificadores de los alumnos
 	id_alumno = 1	
-
+	# se crea el hilo profesor
 	profesor = Profesor(condicion)
-	profesor.daemon = True
-	profesor.start()
+	profesor.daemon = True # para detener el hilo cuando no hayan mas alumnos
+	profesor.start() # se inicializa el hilo
 
 
 	try:
+		# bucle principal
 		while True:
+			#creacion indeterminada de alumnos
 			alumno = Alumno(id_alumno, numero_sillas, turno, condicion)
-			alumno.start()
-			id_alumno += 1
-			time.sleep(3)
-	except KeyboardInterrupt:
+			alumno.start() # inicializacion de cada alumno
+			id_alumno += 1 # conteo de la id de los alumnos
+			time.sleep(3) # retraso en el tiempo para que las salidas en consola sean legibles
+	except KeyboardInterrupt: # ctrl + c para terminar el programa
 		print("finalizacion")
 
 
