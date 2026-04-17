e8508b25d7a0dfdbfcb7df424939661b66fee990
David <david@pc-bd-dda.fi.unam>
2026-04-16 22:47:26 -0600

    Algoritmo RR funcional con metricas

--- tareas/3/ArzateAdrian-DiazDavid/planificadore.py
+++ tareas/3/ArzateAdrian-DiazDavid/planificadore.py
@@ -80,6 +80,70 @@ def FCFS(procesos):
         "Secuencia": secuencia
     }
 
+def RR(procesos, quantum):
+    ticks = 0
+    secuencia = []
+
+    procesos_por_llegar = deque(procesos)
+    procesos_listos = deque()
+
+    procesos_terminados = []
+
+    proceso_actual = None
+    quantum_restante = 0
+
+    while procesos_por_llegar or procesos_listos or proceso_actual:
+
+        while procesos_por_llegar and procesos_por_llegar[0].tiempo_llegada <= ticks:
+            procesos_listos.append(procesos_por_llegar.popleft())
+    
+        if not proceso_actual and procesos_listos:
+            proceso_actual = procesos_listos.popleft()
+            quantum_restante = quantum
+
+        if proceso_actual:
+            secuencia.append(proceso_actual.nombre)
+            proceso_actual.tiempo_restante -= 1
+            quantum_restante -= 1
+        
+            if proceso_actual.tiempo_restante == 0:
+                tiempo_finalizacion = ticks + 1
+
+                proceso_actual.tiempo_retorno = tiempo_finalizacion - proceso_actual.tiempo_llegada
+                proceso_actual.tiempo_espera = proceso_actual.tiempo_retorno - proceso_actual.tiempo_servicio
+
+                procesos_terminados.append(proceso_actual)
+                proceso_actual = None
+
+                proceso_actual = None
+            elif quantum_restante == 0:
+                procesos_listos.append(proceso_actual)
+                proceso_actual = None
+        else:
+            secuencia.append('-')
+    
+        ticks += 1
+    
+    total_procesos = len(procesos_terminados)
+
+    # sum() con generadores para calcular el promedio en una sola línea
+    prom_T = sum(p.tiempo_retorno for p in procesos_terminados) / total_procesos
+    prom_E = sum(p.tiempo_espera for p in procesos_terminados) / total_procesos
+    
+    # La proporción P es (Retorno / Servicio) para cada proceso
+    prom_P = sum(p.tiempo_retorno / p.tiempo_servicio for p in procesos_terminados) / total_procesos
+
+    return {
+        "algoritmo": "RR",
+        "T":prom_T,
+        "E":prom_E,
+        "P":prom_P,
+        "Secuencia": secuencia
+    }
+        
+
+
 procesos_prueba = proceso_aleatorio(5)
 
-print(FCFS(procesos_prueba))
+print(FCFS(copy.deepcopy(procesos_prueba)))
+print(RR(copy.deepcopy(procesos_prueba), 2))
