22d4dc169633163da631b8eb3ce06e308fe6835b
David <david@pc-bd-dda.fi.unam>
2026-04-16 22:19:54 -0600

    Algoritmo FCFS funcional, sin metricas

--- tareas/3/ArzateAdrian-DiazDavid/planificadore.py
+++ tareas/3/ArzateAdrian-DiazDavid/planificadore.py
@@ -28,4 +28,51 @@ def proceso_aleatorio(num_procesos):
     procesos.sort(key=lambda p: p.tiempo_llegada)
     return procesos
 
-print(proceso_aleatorio(5))
+def FCFS(procesos):
+    ticks = 0
+    secuencia = []
+
+    procesos_por_llegar = deque(procesos)
+    procesos_listos = deque()
+
+    procesos_terminados = []
+
+    while procesos_por_llegar or procesos_listos:
+        
+        while procesos_por_llegar and procesos_por_llegar[0].tiempo_llegada <= ticks:
+            procesos_listos.append(procesos_por_llegar.popleft())
+        
+        if procesos_listos:
+            proceso_actual = procesos_listos[0]
+
+            secuencia.append(proceso_actual.nombre)
+
+            proceso_actual.tiempo_restante -= 1
+
+            if proceso_actual.tiempo_restante == 0:
+                
+                tiempo_finalizacion = ticks + 1
+
+                proceso_actual.tiempo_retorno = tiempo_finalizacion - proceso_actual.tiempo_llegada
+                proceso_actual.tiempo_espera = proceso_actual.tiempo_retorno - proceso_actual.tiempo_servicio
+
+                procesos_terminados.append(procesos_listos.popleft())
+            
+        else:
+            secuencia.append('-')
+            
+        ticks += 1    
+    
+    # logica de promedios
+
+    return {
+        "algoritmo": "FCFS",
+        "T":0,
+        "E":0,
+        "P":0,
+        "Secuencia": secuencia
+    }
+
+procesos_prueba = proceso_aleatorio(5)
+
+print(FCFS(procesos_prueba))
